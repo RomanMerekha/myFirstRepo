7f75b0e8421ff03ce5c6d3f762c827f9190973a5
RomanMerekha <merramzes@gmail.com>
2020-02-29 16:53:48 +0200

    dobavil logicheskiy operatot I

---
+++ logical.operators.i.js
@@ -0,0 +1,7 @@
+// Оператор && (И) пишется как два амперсанда &&
+// В традиционном программировании И возвращает true, если оба аргумента истины, а иначе false
+
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
